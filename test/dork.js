28bc62c68b053afa43d36c8b57eb71a9d3acc6da
Lee Olayvar <leeolayvar@gmail.com>
2012-05-16 18:54:18 +0000

    Added mocha for the pre v1 tests. I need my tests dammit! I'll dogfood later.

---
+++ test/dork.js
@@ -0,0 +1,7 @@
+//
+// test/dork.js
+//
+// Copyright (c) 2012 Lee Olayvar <leeolayvar@gmail.com>
+// MIT Licensed 
+//
+/*jshint asi: true, laxcomma: true*/
---
+++ test/index.js
@@ -0,0 +1,10 @@
+//
+// test/index.js
+//
+// Copyright (c) 2012 Lee Olayvar <leeolayvar@gmail.com>
+// MIT Licensed 
+//
+/*jshint asi: true, laxcomma: true*/
+
+
+require('./dork')
