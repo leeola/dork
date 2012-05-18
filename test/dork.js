0d3c54c85eb4c346405fae01c49131fd61677ceb
Lee Olayvar <leeolayvar@gmail.com>
2012-05-18 16:26:12 +0000

    Added boiler to the dork test

--- test/dork.js
+++ test/dork.js
@@ -5,3 +5,12 @@
 // MIT Licensed 
 //
 /*jshint asi: true, laxcomma: true*/
+var should = require('should')
+
+
+// These are here so my IDE will shut the hell up.
+var before = global.before
+  , before_each = global.beforeEach
+  , describe = global.describe
+  , it = global.it
+
