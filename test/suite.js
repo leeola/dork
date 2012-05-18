bce682a961383e306519cc2755a42e6767babaf3
Lee Olayvar <leeolayvar@gmail.com>
2012-05-18 16:26:25 +0000

    Added some tests to suite

--- test/suite.js
+++ test/suite.js
@@ -39,9 +39,113 @@ describe('suite', function () {
       old_suite.foo = 'bar'
       should.not.exist(new_suite.foo)
     })
+    
+    it('should support a single fn', function () {
+      var fn = function () {}
+        , suite = suite_lib(fn)
+      
+      should.not.exist(suite.description)
+      should.not.exist(suite.location)
+      suite.fn.should.equal(fn)
+    })
+    
+    it('should support a description and a fn', function () {
+      var fn = function () {}
+        , description = 'foo'
+        , suite = suite_lib(description, fn)
+      
+      suite.description.should.equal(description)
+      should.not.exist(suite.location)
+      suite.fn.should.equal(fn)
+    })
+    
+    it('should support a description, location, and a fn', function () {
+      var fn = function () {}
+        , description = 'foo'
+        , location = 'bar'
+        , suite = suite_lib(description, location, fn)
+      
+      suite.description.should.equal(description)
+      suite.location.should.equal(location)
+      suite.fn.should.equal(fn)
+    })
   })
   
-  describe('#push()', function () {
+  describe('#add_suite()', function () {
+    var suite
     
+    before_each(function () {
+      suite = suite_lib()
+    })
+    
+    it('should call the given suite\'s fn', function (done) {
+      var new_suite = suite_lib(function () {
+        done()
+      })
+      suite.add_suite(new_suite)
+    })
+    
+    // Here we are checking the timing of the scope assignment in the suite.
+    it('should set the scope to the newly created suite', function () {
+      var fn_called = false
+      
+      var new_suite = suite_lib(function () {
+        suite.scope.should.equal(new_suite)
+        fn_called = true
+      })
+      
+      suite.scope.should.equal(suite)
+      suite.add_suite(new_suite)
+      suite.scope.should.equal(suite)
+      
+      // And just to be safe, lets make sure the new suite's fn was called.
+      // this is tested by an above test, but lets just make sure.
+      fn_called.should.be.true
+    })
+    
+    // This is a rather complex test, so for further explanation please see
+    // the documentation on Suite.add_suite().
+    it('should set the scope to the nested suites', function () {
+      var fn_called = false
+      
+      var suite_a = suite_lib(function () {
+        
+        var suite_b = suite_lib(function () {
+          
+          // Finally it's c!
+          var suite_c = suite_lib(function () {
+            suite.scope.should.equal(suite_c)
+            fn_called = true
+          })
+          
+          // Add c
+          suite.scope.should.equal(suite_b)
+          suite.add_suite(suite_c)
+          suite.scope.should.equal(suite_b)
+        })
+        
+        // Add b
+        suite.scope.should.equal(suite_a)
+        suite.add_suite(suite_b)
+        suite.scope.should.equal(suite_a)
+      })
+      
+      // Add a
+      suite.scope.should.equal(suite)
+      suite.add_suite(suite_a)
+      suite.scope.should.equal(suite)
+      
+      // And just to be safe, lets make sure the new suite's fn was called.
+      // this is tested by an above test, but lets just make sure.
+      fn_called.should.be.true
+    })
+  })
+  
+  describe('#run()', function () {
+    var suite
+    
+    before_each(function () {
+      suite = suite_lib()
+    })
   })
 })
