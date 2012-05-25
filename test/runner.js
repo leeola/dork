4f907eddc35f2f2ffb1d05003c69ba1107523080
Lee Olayvar <leeolayvar@gmail.com>
2012-05-25 04:22:54 +0000

    Added the runner test file.

---
+++ test/runner.js
@@ -0,0 +1,22 @@
+//
+// test/runner.js
+//
+// Copyright (c) 2012 Lee Olayvar <leeolayvar@gmail.com>
+// MIT Licensed 
+//
+/*jshint asi: true, laxcomma: true*/
+var should = require('should')
+
+
+
+// These are here so my IDE will shut the hell up.
+var before = global.before
+  , before_each = global.beforeEach
+  , describe = global.describe
+  , it = global.it
+
+
+describe('runner', function () {
+  var runner_lib = require('../lib/runner')
+  
+})
