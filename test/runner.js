b76dbda33b60cbc61b1f0b347fc54dca2a2cc6e4
Lee Olayvar <leeolayvar@gmail.com>
2012-05-25 04:45:14 +0000

    Added some tests for create()

--- test/runner.js
+++ test/runner.js
@@ -19,4 +19,25 @@ var before = global.before
 describe('runner', function () {
   var runner_lib = require('../lib/runner')
   
+  describe('create()', function () {
+    var runner
+    
+    before_each(function () {
+      runner = runner_lib.create(function () {})
+    })
+    
+    it('should return a runner object instance', function () {
+      // To assert it's identity we're just going to make sure
+      // some basic test functions exist.
+      should.exist(runner._fn)
+    })
+    
+    it('should be a unique object each execution', function () {
+      var new_runner = runner_lib.create()
+        , old_runner = runner
+      
+      old_runner.foo = 'bar'
+      should.not.exist(new_runner.foo)
+    })
+  })
 })
