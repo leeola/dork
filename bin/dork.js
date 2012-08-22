f46c94951f7bb21dab4a355e66f9aa4e69803925
Lee Olayvar <leeolayvar@gmail.com>
2012-08-22 15:16:31 -0400

    Added a basic command line interface to Dork tests.

---
+++ bin/dork.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+//
+// bin/dork.js
+//
+// The bin Dork.js entry point.
+//
+// Note that this file is in JavaScript because CoffeeScript is currently
+// unable to properly add hashbangs as this file has.
+//
+// Copyright (c) 2012 Lee Olayvar <leeolayvar@gmail.com>
+// MIT Licensed
+//
+require('../lib/bin').exec();
---
+++ bin/index.coffee
@@ -0,0 +1,10 @@
+#
+# bin/index.coffee
+#
+# This little index file allows you to call `coffee bin` from the project
+# directory. This is just a tiny dev tool, but handy nonetheless.
+#
+# Copyright (c) 2012 Lee Olayvar <leeolayvar@gmail.com>
+# MIT Licensed
+#
+require './dork'
